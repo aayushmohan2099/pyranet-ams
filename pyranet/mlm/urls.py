75a9ea6f0f6c380e930ca13cd64fc68ee86585bd
Ayush_M <ayushmohan193@gmail.com>
2024-07-01 03:35:15 +0530

    Modified Member Detail Views | Added HomeScreen | Added User Network Graphs

--- pyranet/mlm/urls.py
+++ pyranet/mlm/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('member/<int:member_id>/', views.member_detail, name='member_detail'),
     path('create-user/', views.create_user, name='create_user'),
     path('user-created/<str:username>/<int:member_id>/', views.user_created, name='user_created'),
+    path('', views.homepage, name='homepage'),
+    path('find-member/', views.find_member, name='find_member'),
+    # path('member/<int:member_id>/network/', views.member_network, name='member_network'),
 ]
--- pyranet/mlm/views.py
+++ pyranet/mlm/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Member
 from .forms import UserCreationForm
+import json
+from django.http import JsonResponse
+
+def homepage(request):
+    members = Member.objects.all()
+    return render(request, 'homepage.html', {'members':members})
+
+def find_member(request):
+    member_id = request.GET.get('member_id')
+    member = get_object_or_404(Member, id=member_id)
+    return redirect('member_detail', member_id=member.id)
 
 def member_detail(request, member_id):
     member = Member.objects.get(id=member_id)
-    return render(request, 'member_detail.html', {'member': member})
+    members = Member.objects.all()
+
+    def get_network_tree(member):
+        # Recursive function to build the tree structure
+        children = []
+        for child_relationship in member.children.all():
+            children.append(get_network_tree(child_relationship.child))
+        
+        return {
+            'name': member.user.username,
+            'children': children
+        }
+
+    network_data = get_network_tree(member)
+    network_tree_html = render_network_tree(network_data)
+
+    return render(request, 'member_detail.html', {'member': member, 'members': members, 'network_tree': network_tree_html})
 
 def create_user(request):
     if request.method == 'POST':
@@ -21,3 +48,16 @@ def create_user(request):
 def user_created(request, username, member_id):
     member = Member.objects.get(id=member_id)
     return render(request, 'user_created.html', {'username': username, 'member': member})
+
+def render_network_tree(node):
+    # Recursive rendering of the tree structure
+    html = f'<li><a href="javascript:void(0);"><div class="member-view-box"><div class="member-image"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg><div class="member-details"><h3>{node["name"]}</h3></div></div></div></a>'
+    
+    if node.get('children'):
+        html += '<ul>'
+        for child in node['children']:
+            html += render_network_tree(child)
+        html += '</ul>'
+    
+    html += '</li>'
+    return html
